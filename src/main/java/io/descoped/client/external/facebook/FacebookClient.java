4d2ef124d7121fa86a34975ab9191148837a8d71
oranheim <oranheim@gmail.com>
2017-11-22 07:51:36 +0100

    fixed a missing url encoding for fb places api query

--- src/main/java/io/descoped/client/external/facebook/FacebookClient.java
+++ src/main/java/io/descoped/client/external/facebook/FacebookClient.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 
@@ -20,7 +22,7 @@ public class FacebookClient extends FacebookConnect {
     // https://developers.facebook.com/docs/graph-api/using-graph-api/v2.11#search
     public String getPageList(String query, Double lat, Double lng, Integer radius) throws UnsupportedEncodingException {
         String fbURI = "/search?%stype=place&center=%s,%s&distance=%s&fields=%s"; // query, lat/long, radius
-        String q = (query != null ? "q="+query+"&" : "");
+        String q = (query != null ? "q=" + URLEncoder.encode(query, StandardCharsets.UTF_8.name()) + "&" : "");
         fbURI = String.format(fbURI, q, lat, lng, radius, "id,name,location,category_list,genre,hometown");
         log.trace("fbURI: {}", fbURI);
 
