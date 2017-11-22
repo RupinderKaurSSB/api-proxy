a7824247f86ddc947f5d16ac42feab5ef051b310
oranheim <oranheim@gmail.com>
2017-11-22 10:20:11 +0100

    made a test case for common util

--- src/test/java/io/descoped/client/util/test/CommonUtilTest.java
+++ src/test/java/io/descoped/client/util/test/CommonUtilTest.java
@@ -2,18 +2,31 @@ package io.descoped.client.util.test;
 
 import io.descoped.client.util.CommonUtil;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
 
 /**
  * @author Ove Ranheim (oranheim@gmail.com)
  * @since 22/11/2017
  */
 public class CommonUtilTest {
+
+    private static final Logger log = LoggerFactory.getLogger(CommonUtilTest.class);
+
+    @Test
+    public void testClassloader() throws Exception {
+        assertThat(CommonUtil.tccl()).isEqualTo(Thread.currentThread().getContextClassLoader());
+    }
 
     @Test
     public void testStreams() throws Exception {
@@ -33,4 +46,29 @@ public class CommonUtilTest {
 
         CommonUtil.closeAndCreateNewOutputStream(out);
     }
+
+    @Test
+    public void testMockLog() throws Exception {
+        assertThat(CommonUtil.fileSeparator).isNotBlank();
+
+        CommonUtil mock = Mockito.mock(CommonUtil.class);
+        spy(mock).info("foo");
+        spy(mock).debug("foo");
+        spy(mock).trace("foo");
+        spy(mock).warn("foo");
+        spy(mock).error("foo");
+
+        assertThat(mock.getCurrentPath()).exists();
+        assertThat(mock.currentPath()).isEqualTo(mock.getCurrentPath().toString());
+
+        List untypedStrings = new ArrayList();
+        untypedStrings.add("foo");
+        untypedStrings.add("bar");
+        untypedStrings.add("foobar");
+
+        List<String> typedStrings = new ArrayList();
+        typedStrings = CommonUtil.typedList(untypedStrings, String.class, typedStrings.getClass());
+        assertThat(untypedStrings).isEqualTo(typedStrings);
+    }
+
 }
---
+++ src/test/java/io/descoped/client/util/test/StaticCommonUtilTest.java
@@ -0,0 +1,24 @@
+package io.descoped.client.util.test;
+
+import org.junit.Test;
+
+/**
+ * @author Ove Ranheim (oranheim@gmail.com)
+ * @since 22/11/2017
+ */
+public class StaticCommonUtilTest {
+
+    @Test
+    public void testOtherMethods() throws Exception {
+//        CommonUtil mock = Mockito.mock(CommonUtil.class);
+//        when(mock.getCurrentPath()).thenReturn(Paths.get("/tmp"));
+
+//        PowerMockito.mockStatic(CommonUtil.class);
+//        OngoingStubbing<Path> path = PowerMockito.when(CommonUtil.getCurrentPath()).thenReturn(Paths.get("/tmp"));
+//        assertThat(path).toString().equals("/tmp");
+
+//        CommonUtil mock = spy(new CommonUtil());
+//        doReturn(Paths.get("/tmp")).when(mock).invokesStatic(CommonUtil.class).makeString();
+//        assertThat(mock.bar()).isEqualTo("foo");
+    }
+}
