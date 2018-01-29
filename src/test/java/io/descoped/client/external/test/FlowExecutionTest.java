f516cc3d7a3f6cff96cddf5d9a63397c9721f374
oranheim <oranheim@gmail.com>
2018-01-29 16:23:01 +0100

    disabled google and facebook test cases

--- src/test/java/io/descoped/client/external/test/FlowExecutionTest.java
+++ src/test/java/io/descoped/client/external/test/FlowExecutionTest.java
@@ -5,6 +5,7 @@ import io.descoped.client.external.google.GeoLocation;
 import io.descoped.client.external.google.GoogleMapsClient;
 import io.descoped.client.external.posten.PostalCode;
 import io.descoped.client.external.posten.PostenPostalCodesClient;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +21,7 @@ public class FlowExecutionTest {
     private static final Logger log = LoggerFactory.getLogger(FlowExecutionTest.class);
 
     @Test
+    @Ignore
     public void should_execute_flow() throws Exception {
         PostenPostalCodesClient postenClient = new PostenPostalCodesClient();
         postenClient.fetch();
